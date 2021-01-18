01bb347524f0e8b84025a8ea886fcbbb2955ed9e
Ed Kelly <me@edkelly.co.uk>
2021-01-18 15:57:35 +0000

    Monorepo CircleCI

--- apps/pwa/src/components/ArticleListItem.js
+++ apps/pwa/src/components/ArticleListItem.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { inject, observer } from 'mobx-react';
 import { withStyles } from '@material-ui/core/styles';
 import Collapse from '@material-ui/core/Collapse';
+import Grid from '@material-ui/core/Grid';
 import ListItem from '@material-ui/core/ListItem';
 import Link from '@material-ui/core/Link';
 import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import { Media } from 'react-media-player';
 import ArticlePlayer from './ArticlePlayer';
@@ -48,36 +48,37 @@ class ArticleListItem extends React.Component {
         in={item.deleted ? false : true}
       >
         <ListItem>
-          <Link
-            href={item.decodedUrl}
-            target="_new"
+          <Grid
+            container
           >
-            <ListItemText
-              className={classes.text}
-              primary={`${item.siteName} - ${item.title}`}
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="body2"
-                    className={classes.inline}
-                    color="textPrimary"
-                  >
-                    {`${timeString} ${dateString}`}
-                  </Typography>
-                  <br/>
-                  {item.decodedUrl}
-                </React.Fragment>
-              }
-            />
-          </Link>
-          <ListItemSecondaryAction>
-            <Media>
-              <ArticlePlayer
-                item={item}
-              />
-            </Media>
-          </ListItemSecondaryAction>
+            <Grid
+              item
+              xs={7}
+            >
+              <Link
+                href={item.decodedUrl}
+                target="_new"
+              >
+                <ListItemText
+                  className={classes.text}
+                  primary={`${item.siteName} - ${item.title}`}
+                  secondary={`${timeString} ${dateString}`}
+                />
+              </Link>
+            </Grid>
+            <Grid
+              item
+              xs={5}
+            >
+              <ListItemSecondaryAction>
+                <Media>
+                  <ArticlePlayer
+                    item={item}
+                  />
+                </Media>
+              </ListItemSecondaryAction>
+            </Grid>
+          </Grid>
         </ListItem>
       </Collapse>
     );
